e446bf6d926706359f6fb87aa2cf53e04fe3e02b
krzychu <krzysztof.nowak2005@gmail.com>
2025-11-13 14:55:50 +0100

    deleting unused stuff

--- compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -f CatFights.class
-javac --module-path "./javafx-sdk-25.0.1/lib" --add-modules javafx.controls,javafx.fxml CatFights.java
--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java --module-path "./javafx-sdk-25.0.1/lib" --add-modules javafx.controls,javafx.fxml CatFights
